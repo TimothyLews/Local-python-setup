b3119355d2972a83fef46837eb796dfc303e9b2b
TimothyLewis <timlew2003@gmail.com>
2024-10-15 19:58:26 -0700

    Add test cases for hello, pack, and eat_lunch functions

--- python_projects/functions_practice.py
+++ python_projects/functions_practice.py
@@ -13,3 +13,10 @@ def hello():
         print(f"First I eat {my_lst[0]}")
       else:
         print(f"Next I eat {my_lst[i]}")
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
